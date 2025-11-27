df09d8dff670f34e8af6dc9a2a89c45661aa41b9
xiaoyu1738 <xiaoyu1738jw@gmail.com>
2025-11-27 13:37:38 +0800

    Update constants.ts

--- novel.smaiclub.top/src/constants.ts
+++ novel.smaiclub.top/src/constants.ts
@@ -5,162 +5,162 @@ export const BOOKS: Novel[] = [
     "id": "classroom-of-the-elite",
     "title": "欢迎来到实力至上主义的教室",
     "author": "衣笠彰梧",
-    "coverUrl": "/epubs/欢迎来到实力至上主义教室/cover.png",
+    "coverUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/cover.png",
     "volumes": [
       {
         "id": 0,
         "title": "第 0 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2000.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2000.epub"
       },
       {
         "id": 1,
         "title": "第 1 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2001.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2001.epub"
       },
       {
         "id": 2,
         "title": "第 2 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2002.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2002.epub"
       },
       {
         "id": 3,
         "title": "第 3 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2003.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2003.epub"
       },
       {
         "id": 4,
         "title": "第 4 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2004.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2004.epub"
       },
       {
         "id": 5,
         "title": "第 4.5 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%204.5.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%204.5.epub"
       },
       {
         "id": 6,
         "title": "第 5 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2005.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2005.epub"
       },
       {
         "id": 7,
         "title": "第 6 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2006.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2006.epub"
       },
       {
         "id": 8,
         "title": "第 7 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2007.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2007.epub"
       },
       {
         "id": 9,
         "title": "第 7.5 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%207.5.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%207.5.epub"
       },
       {
         "id": 10,
         "title": "第 8 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2008.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2008.epub"
       },
       {
         "id": 11,
         "title": "第 9 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2009.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2009.epub"
       },
       {
         "id": 12,
         "title": "第 10 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2010.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2010.epub"
       },
       {
         "id": 13,
         "title": "第 11 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2011.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2011.epub"
       },
       {
         "id": 14,
         "title": "第 11.5 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2011.5.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2011.5.epub"
       },
       {
         "id": 15,
         "title": "第 12 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2012.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2012.epub"
       },
       {
         "id": 16,
         "title": "第 13 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2013.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2013.epub"
       },
       {
         "id": 17,
         "title": "第 14 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2014.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2014.epub"
       },
       {
         "id": 18,
         "title": "第 15 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2015.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2015.epub"
       },
       {
         "id": 19,
         "title": "第 15.5 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2015.5.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2015.5.epub"
       },
       {
         "id": 20,
         "title": "第 16 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2016.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2016.epub"
       },
       {
         "id": 21,
         "title": "第 17 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2017.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2017.epub"
       },
       {
         "id": 22,
         "title": "第 18 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2018.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2018.epub"
       },
       {
         "id": 23,
         "title": "第 19 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2019.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2019.epub"
       },
       {
         "id": 24,
         "title": "第 20 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2020.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2020.epub"
       },
       {
         "id": 25,
         "title": "第 20.5 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2020.5.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2020.5.epub"
       },
       {
         "id": 26,
         "title": "第 21 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2021.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2021.epub"
       },
       {
         "id": 27,
         "title": "第 22 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2022.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2022.epub"
       },
       {
         "id": 28,
         "title": "第 23 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2023.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2023.epub"
       },
       {
         "id": 29,
         "title": "第 23.5 卷",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2023.5.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20-%2023.5.epub"
       },
       {
         "id": 30,
         "title": "短篇 01",
-        "epubUrl": "/epubs/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20短篇%20-%2001.epub"
+        "epubUrl": "https://lib.smaiclub.top/欢迎来到实力至上主义教室/欢迎来到实力至上主义的教室%20短篇%20-%2001.epub"
       }
     ]
   },
@@ -168,37 +168,37 @@ export const BOOKS: Novel[] = [
     "id": "to-be-a-power-in-the-shadows",
     "title": "想要成为影之实力者",
     "author": "逢沢大介",
-    "coverUrl": "/epubs/想要成为影之实力者/cover.jpg",
+    "coverUrl": "https://lib.smaiclub.top/想要成为影之实力者/cover.jpg",
     "volumes": [
       {
           "id": 1,
           "title": "第 1 卷",
-          "epubUrl": "/epubs/想要成为影之实力者/想要成为影之实力者1.epub"
+          "epubUrl": "https://lib.smaiclub.top/想要成为影之实力者/想要成为影之实力者1.epub"
       },
       {
           "id": 2,
           "title": "第 2 卷",
-          "epubUrl": "/epubs/想要成为影之实力者/想要成为影之实力者2.epub"
+          "epubUrl": "https://lib.smaiclub.top/想要成为影之实力者/想要成为影之实力者2.epub"
       },
       {
           "id": 3,
           "title": "第 3 卷",
-          "epubUrl": "/epubs/想要成为影之实力者/想要成为影之实力者3.epub"
+          "epubUrl": "https://lib.smaiclub.top/想要成为影之实力者/想要成为影之实力者3.epub"
       },
       {
           "id": 4,
           "title": "第 4 卷",
-          "epubUrl": "/epubs/想要成为影之实力者/想要成为影之实力者4.epub"
+          "epubUrl": "https://lib.smaiclub.top/想要成为影之实力者/想要成为影之实力者4.epub"
       },
       {
           "id": 5,
           "title": "第 5 卷",
-          "epubUrl": "/epubs/想要成为影之实力者/想要成为影之实力者5.epub"
+          "epubUrl": "https://lib.smaiclub.top/想要成为影之实力者/想要成为影之实力者5.epub"
       },
       {
           "id": 6,
           "title": "第 6 卷",
-          "epubUrl": "/epubs/想要成为影之实力者/想要成为影之实力者6.epub"
+          "epubUrl": "https://lib.smaiclub.top/想要成为影之实力者/想要成为影之实力者6.epub"
       }
     ]
   }
